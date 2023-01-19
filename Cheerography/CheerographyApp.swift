8da4be2747f47b88bd6c29969306b85f26452e0b
AngelusGF <icheercanada@gmail.com>
2023-01-18 22:56:08 -0800

    Initial Commit

---
+++ Cheerography/CheerographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerographyApp.swift
+//  Cheerography
+//
+//  Created by Angelus Gerlich-Fitzgerald on 2023-01-18.
+//
+
+import SwiftUI
+
+@main
+struct CheerographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cheerography/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cheerography
+//
+//  Created by Angelus Gerlich-Fitzgerald on 2023-01-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
