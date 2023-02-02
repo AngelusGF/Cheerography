fa474131adbb6a6c3d9e23dc8be204a97add7acf
AngelusGF <icheercanada@gmail.com>
2023-02-01 19:43:28 -0800

    update!

--- Cheerography/ContentView.swift
+++ Cheerography/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            LazyVGrid(columns: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Columns@*/[GridItem(.fixed(200))]/*@END_MENU_TOKEN@*/) {
+                ControlGroup {
+                    Button("Add Formation") {
+                        //action in here
+                    }
+                    //.position(x: 50.0, y: 20.0)
+                        
+                    Button("Add People") {
+                        //action in here
+                    }
+                    //.position(x: 100.0, y:20.0)
+                        
+                    Button("Run Routine") {
+                        //action in here
+                    }
+                    //.position(x: 150.0, y: 20.0)
+                   
+                }
+            }
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.landscapeRight)
+        }
     }
 }
