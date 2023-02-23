07a1d72298d14bb9813e21e3d3552b9f0f3a117c
AngelusGF <icheercanada@gmail.com>
2023-02-22 19:55:21 -0800

    Feb 22nd Commit

--- Cheerography/ContentView.swift
+++ Cheerography/ContentView.swift
@@ -9,36 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Group {
-            LazyVGrid(columns: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Columns@*/[GridItem(.fixed(200))]/*@END_MENU_TOKEN@*/) {
-                ControlGroup {
-                    Button("Add Formation") {
-                        //action in here
-                    }
-                    //.position(x: 50.0, y: 20.0)
-                        
-                    Button("Add People") {
-                        //action in here
-                    }
-                    //.position(x: 100.0, y:20.0)
-                        
-                    Button("Run Routine") {
-                        //action in here
-                    }
-                    //.position(x: 150.0, y: 20.0)
-                   
+                
+        VStack(alignment: .center) {
+            
+            Spacer()
+            
+            //Element 1/10
+            HStack() {
+                Button("Add Cheerio") {
+                    //function go here
                 }
+                .padding(10.0)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+               
+                Button("Run Routine") {
+                    //function go here
+                }
+                .padding(10.0)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10.0)
+               
+                Button("Save") {
+                    //function go here
+                }
+                .padding(10.0)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10.0)
+                
+                Button("Add Music") {
+                    //function go here
+                }
+                .padding(10.0)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10.0)
+                
             }
+
+            //Element 2/10
+            Image("BlueFloor")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+            
+            Spacer()
+            
         }
+        
+        .padding(.all, 10.0)
+        .background(Color.black)
+        
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView()
-                .previewInterfaceOrientation(.landscapeRight)
-        }
+        ContentView()
+            .padding(0.0)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+.previewInterfaceOrientation(.portraitUpsideDown)
+            
     }
 }
